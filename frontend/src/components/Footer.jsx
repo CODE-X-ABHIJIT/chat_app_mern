253037ee31d743e980bcad0f0a97839e274f1016
CODE-X-ABHIJIT <158609749+CODE-X-ABHIJIT@users.noreply.github.com>
2024-12-29 09:20:55 +0530

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 
 export const Footer = () => {
   return (
-    <div class="bg-gray-800 text-gray-300 py-3">
-    <div class="container mx-auto text-center">
-        <span class="text-xs sm:text-sm md:text-base">
-            Created By <a href="https://code-x-abhijit.github.io/portfolio/" class="text-blue-400 hover:underline">Abhijit</a> | &#169; 2024 All Rights Reserved
+    <div className="bg-gray-800 text-gray-300 py-3">
+    <div className="container mx-auto text-center">
+        <span className="text-xs sm:text-sm md:text-base">
+            Created By <a href="https://code-x-abhijit.github.io/portfolio/" className="text-blue-400 hover:underline">Abhijit</a> | &#169; 2024 All Rights Reserved
         </span>
     </div>
 </div>
