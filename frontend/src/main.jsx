f054ef0f7941f46306b8be20952aba46a595573e
CODE-X-ABHIJIT <codexabhijit@gmail.com>
2024-12-29 07:39:40 +0530

    new

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App.jsx";
 
 import { BrowserRouter } from "react-router-dom";
-import Footer from "./components/footer.jsx";
+import Footer from "./components/Footer.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
