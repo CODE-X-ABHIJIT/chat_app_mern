7ffc5bffb24817635386579abce8db2012da5c21
CODE-X-ABHIJIT <158609749+CODE-X-ABHIJIT@users.noreply.github.com>
2024-12-30 20:09:11 +0530

    Update auth.middleware.js

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -4,7 +4,7 @@ import User from "../models/user.model.js";
 export const protectRoute = async (req, res, next) => {
   try {
     const token = req.cookies.tok;
-    console.log(tok);
+    console.log(req.cookies);
     if (!token) {
       return res.status(401).json({ message: "Unauthorized - No Token Provided" });
     }
