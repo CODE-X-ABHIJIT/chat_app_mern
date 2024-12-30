ec539a0f0259cde1bf01b8bc586d87509a08b21b
CODE-X-ABHIJIT <158609749+CODE-X-ABHIJIT@users.noreply.github.com>
2024-12-30 20:35:08 +0530

    Update utils.js

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -4,7 +4,7 @@ export const generateToken = (userId, res) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: "7d",
   });
-  res.cookie("tok", token, {
+  res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000, // MS
     httpOnly: true, // prevent XSS attacks cross-site scripting attacks
     sameSite: "strict", // CSRF attacks cross-site request forgery attacks
