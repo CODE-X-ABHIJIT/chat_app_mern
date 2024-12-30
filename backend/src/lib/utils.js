3936bf0bfd67f932f3f4e39f77bb4d54c5fc70fb
CODE-X-ABHIJIT <158609749+CODE-X-ABHIJIT@users.noreply.github.com>
2024-12-30 17:16:14 +0530

    Update utils.js

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -2,7 +2,6 @@ import jwt from "jsonwebtoken";
 
 export const generateToken = (userId, res) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-    console.log("JWT_SECRET:", process.env.JWT_SECRET);
     expiresIn: "7d",
   });
 
