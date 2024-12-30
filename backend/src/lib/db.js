910de260ba39912e19d423222a131a392d97c4fd
CODE-X-ABHIJIT <158609749+CODE-X-ABHIJIT@users.noreply.github.com>
2024-12-30 18:53:21 +0530

    Update db.js

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -5,7 +5,6 @@ export const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGODB_URI);
     console.log(`MongoDB connected: ${conn.connection.host}`);
      console.log("JWT_SECRET:", process.env.JWT_SECRET);
-    console.log("Required cookie", req.cookies);
   } catch (error) {
     console.log("MongoDB connection error:", error);
   }
