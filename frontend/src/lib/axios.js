99d4ea632106fae3fe4ef4cf1c3c0858371fff6f
CODE-X-ABHIJIT <158609749+CODE-X-ABHIJIT@users.noreply.github.com>
2024-12-30 17:10:18 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001" : "https://chat-app-mern-pkem.onrender.com",
+  baseURL: "https://chat-app-mern-pkem.onrender.com",
   withCredentials: true,
 });
